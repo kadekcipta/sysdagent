b22b3c46a16510f5358279f08988bab4d346f4fa
cipta <kadekcipta@gmail.com>
2015-07-28 13:24:00 +0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,238 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	"github.com/godbus/dbus"
+)
+
+const (
+	envServices = "X_SYSD_SERVICES"
+
+	propertiesChanged = "org.freedesktop.DBus.Properties.PropertiesChanged"
+	propGet           = "org.freedesktop.DBus.Properties.Get"
+)
+
+type serviceStatus struct {
+	ID                     string
+	Name                   string
+	Path                   dbus.ObjectPath
+	ActiveState            string
+	ActiveEnterTimestamp   uint64
+	ActiveExitTimestamp    uint64
+	InactiveEnterTimestamp uint64
+	InactiveExitTimestamp  uint64
+}
+
+type unitMonitor struct {
+	name    string
+	done    chan struct{}
+	chanPub chan serviceStatus
+}
+
+func (m *unitMonitor) path() dbus.ObjectPath {
+
+	var ret dbus.ObjectPath
+
+	c, err := dbus.SystemBus()
+	if err != nil {
+		return ret
+	}
+
+	obj := c.Object("org.freedesktop.systemd1", "/org/freedesktop/systemd1")
+	call := obj.Call("org.freedesktop.systemd1.Manager.GetUnit", 0, m.name)
+	if call != nil && call.Err != nil {
+		log.Println(err)
+		return ret
+	}
+
+	if err := call.Store(&ret); err != nil {
+		log.Println(err)
+		return ret
+	}
+
+	return ret
+}
+
+func (m *unitMonitor) getProp(name string) dbus.Variant {
+
+	var ret dbus.Variant
+
+	c, err := dbus.SystemBus()
+	if err != nil {
+		log.Println(err)
+		return ret
+	}
+
+	err = c.Object("org.freedesktop.systemd1", m.path()).Call(
+		propGet,
+		0,
+		"org.freedesktop.systemd1.Unit",
+		name,
+	).Store(&ret)
+
+	if err != nil {
+		panic(err)
+	}
+	return ret
+}
+
+func (m *unitMonitor) generateStatus() serviceStatus {
+	status := serviceStatus{
+		ID:   getLocalIP(),
+		Path: m.path(),
+	}
+
+	var prop dbus.Variant
+	prop = m.getProp("ActiveState")
+	if prop.Value() != nil {
+		status.ActiveState = prop.Value().(string)
+	}
+
+	prop = m.getProp("ActiveEnterTimestamp")
+	if prop.Value() != nil {
+		status.ActiveEnterTimestamp = prop.Value().(uint64)
+	}
+
+	prop = m.getProp("ActiveExitTimestamp")
+	if prop.Value() != nil {
+		status.ActiveExitTimestamp = prop.Value().(uint64)
+	}
+
+	prop = m.getProp("InactiveEnterTimestamp")
+	if prop.Value() != nil {
+		status.InactiveEnterTimestamp = prop.Value().(uint64)
+	}
+
+	prop = m.getProp("InactiveExitTimestamp")
+	if prop.Value() != nil {
+		status.InactiveExitTimestamp = prop.Value().(uint64)
+	}
+
+	return status
+}
+
+func (m *unitMonitor) watch() error {
+
+	conn, err := dbus.SystemBus()
+	if err != nil {
+		return err
+	}
+
+	// subscribe the props changes signal
+	props := fmt.Sprintf("type='signal',path_namespace='%s',interface='org.freedesktop.DBus.Properties'", m.path())
+	call := conn.BusObject().Call("org.freedesktop.DBus.AddMatch", 0, props)
+	if call != nil && call.Err != nil {
+		return call.Err
+	}
+
+	// unsubscribe on return
+	defer func() {
+		conn.BusObject().Call("org.freedesktop.DBus.RemoveMatch", 0, props)
+	}()
+
+	signal := make(chan *dbus.Signal, 10)
+	defer close(signal)
+
+	conn.Signal(signal)
+
+	log.Printf("watching for %s @%s\n", m.name, m.path())
+
+	for {
+		select {
+		case ev := <-signal:
+			switch ev.Name {
+			case propertiesChanged:
+				m.chanPub <- m.generateStatus()
+			}
+
+		case <-m.done:
+			return nil
+
+		}
+	}
+
+	return nil
+}
+
+func waitForOsSignal() {
+	signals := []os.Signal{syscall.SIGINT, syscall.SIGKILL, syscall.SIGTERM, syscall.SIGSTOP}
+	signalSink := make(chan os.Signal, 1)
+
+	defer close(signalSink)
+
+	signal.Notify(signalSink, signals...)
+	<-signalSink
+}
+
+func getLocalIP() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return ""
+	}
+	for _, address := range addrs {
+		// check the address type and if it is not a loopback the display it
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+	return ""
+}
+
+func watchServices(chanDone chan struct{}, units ...string) {
+	chanPub := make(chan serviceStatus, 100)
+	defer close(chanPub)
+
+	for _, unit := range units {
+		go func(u string) {
+			err := (&unitMonitor{
+				done:    chanDone,
+				name:    u,
+				chanPub: chanPub,
+			}).watch()
+			if err != nil {
+				log.Println(u, err)
+			}
+		}(unit)
+	}
+
+	for {
+		select {
+		case status := <-chanPub:
+			log.Println(status)
+		case <-chanDone:
+			return
+		}
+	}
+}
+
+func main() {
+	done := make(chan struct{})
+	defer close(done)
+
+	// sample services
+	services := []string{"docker.service", "redis.service"}
+
+	ose := os.Getenv(envServices)
+	if ose != "" {
+		services = []string{}
+		for _, s := range strings.Split(ose, ",") {
+			services = append(services, strings.TrimSpace(s))
+		}
+	}
+
+	if len(services) == 0 {
+		os.Exit(-1)
+	}
+
+	go watchServices(done, services...)
+	waitForOsSignal()
+}
